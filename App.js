a6fe02783fe2cf4eabbeb83aad3d19268f611ebb
ElianRenteria <elianrenteriadevelopment@gmail.com>
2023-09-18 22:44:30 -0700

    Set up NativeBase

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+//import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box } from "native-base";
+import { Button } from "native-base";
+import { Center, Square, Circle } from 'native-base';
+import { Flex, Spacer } from "native-base";
+
+//Using NativeBase for UI
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Box>Hello world</Box>
+        <Box alignItems="center">
+          <Button onPress={() => console.log("hello world")}>Click Me</Button>
+        </Box>   
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
+
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
