478ec7236afc6ca86f87da143c63f0bac5e3e167
ElianRenteria <elianrenteriadevelopment@gmail.com>
2023-10-08 21:27:45 -0700

    dependencies

--- App.js
+++ App.js
@@ -1,33 +1,28 @@
-//import { StatusBar } from 'expo-status-bar';
-//import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
 import { NativeBaseProvider, Box } from "native-base";
 import { Button } from "native-base";
 import { Center, Square, Circle } from 'native-base';
 import { Flex, Spacer } from "native-base";
+import { Menu } from 'native-base';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
-//Using NativeBase for UI
+//Use NativeBase for UI
 
 export default function App() {
-  
   return (
-    <NativeBaseProvider>
-      <Center flex={1}>
-        <Box>Hello world</Box>
-        <Box alignItems="center">
-          <Button onPress={() => console.log("hello world")}>Click Me</Button>
-        </Box>   
-      </Center>
-    </NativeBaseProvider>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
   );
 }
 
-
-/*const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});*/
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
