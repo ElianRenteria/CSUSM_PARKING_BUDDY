3005e5080953bf812e07a587e25c68340093b320
Jannis <jannisbressan@web.de>
2023-10-19 18:02:39 -0700

    Added a switch to preferences page which changes the text

--- screens/PreferencesScreen.js
+++ screens/PreferencesScreen.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Switch, StyleSheet } from 'react-native';
 
-const PreferencesScreen = () => (
+
+
+const PreferencesScreen = () => {
+
+  const [isWorking, setIsWorking] = useState(true);
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  return( 
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Preferences Screen</Text>
+    <Text>Dark Mode {isWorking ? 'not working' : '10% working'}</Text>
+    <Switch
+        trackColor={{false: '#81b0ff', true: '#007AC3'}}
+        thumbColor={'#f4f3f4'}
+        backgroundColor="ffffff"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+        onChange= {() => {
+          if (isEnabled == true)
+            setIsWorking(true);
+          else if (isEnabled == false)
+            setIsWorking(false);
+        }}
+      />
   </View>
-);
+  )
+};
 
 export default PreferencesScreen;
