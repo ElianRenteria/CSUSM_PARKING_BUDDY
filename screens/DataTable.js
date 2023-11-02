e578c7922959fe9d402b63b4c4c99815b4c05144
Jannis <jannisbressan@web.de>
2023-11-01 22:04:48 -0700

    Added potential Firebase support for the table

--- screens/DataTable.js
+++ screens/DataTable.js
@@ -3,6 +3,18 @@ import React from 'react';
 import { StyleSheet } from 'react-native'; 
 import { DataTable } from 'react-native-paper'; 
 
+/*var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://console.firebase.google.com/u/0/project/parkingbuddy-79e1c/firestore/data/~2FParking%20Structure~2FLot%20F"
+});
+
+const db = admin.firestore();*/
+
+
 OccupancyBCurrent = 200;
 OccupancyCCurrent = 400;
 OccupancyFCurrent = 12;
@@ -16,7 +28,36 @@ OccupancyKMax = 178;
 OccupancyXYZMax = 816;
 OccupancyPS1Max = 1419;
 
-colorPick = (OccCurrent, OccMax) => {
+OccupationDetermine = (Lot) =>{
+
+  //when firebase works in expo use that instead of ifs:
+  //const docRef = db.collection('Parking Structure').doc(Lot);
+  //docRef.get()
+
+  //occupation = doc.data()['OccupationCurrent']
+  if (Lot == 'Lot B'){
+    occupation = 150;
+  }
+  if (Lot == 'Lot C'){
+    occupation = 400;
+  }
+  if (Lot == 'Lot F'){
+    occupation = 643;
+  }
+  if (Lot == 'Lot K'){
+    occupation = 20;
+  }
+  if (Lot == 'Lot XYZ'){
+    occupation = 345;
+  }
+  if (Lot == 'Lot PS1'){
+    occupation = 700;
+  }
+
+  return occupation
+} 
+colorPick = (OccCurrent, OccMax) => {  
+  
   if ((OccCurrent/OccMax) <= 0.33){
     return "green"
   }
@@ -40,34 +81,34 @@ const Table = () => {
       </DataTable.Header> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot B</DataTable.Cell> 
-        <DataTable.Cell>{OccupancyBCurrent}/{OccupancyBMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupancyBCurrent,OccupancyBMax)}></DataTable.Cell>  
+        <DataTable.Cell>{OccupationDetermine('Lot B')}/{OccupancyBMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot B'),OccupancyBMax)}></DataTable.Cell>  
       </DataTable.Row> 
   
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot C</DataTable.Cell> 
-        <DataTable.Cell>{OccupancyCCurrent}/{OccupancyCMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupancyCCurrent,OccupancyCMax)} ></DataTable.Cell>   
+        <DataTable.Cell>{OccupationDetermine('Lot C')}/{OccupancyCMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot C'),OccupancyCMax)} ></DataTable.Cell>   
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot F</DataTable.Cell> 
-        <DataTable.Cell>{OccupancyFCurrent}/{OccupancyFMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupancyFCurrent,OccupancyFMax)} ></DataTable.Cell>  
+        <DataTable.Cell>{OccupationDetermine('Lot F')}/{OccupancyFMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot F'),OccupancyFMax)} ></DataTable.Cell>  
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot K</DataTable.Cell> 
-        <DataTable.Cell>{OccupancyKCurrent}/{OccupancyKMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupancyKCurrent,OccupancyKMax)} ></DataTable.Cell> 
+        <DataTable.Cell>{OccupationDetermine('Lot K')}/{OccupancyKMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot K'),OccupancyKMax)} ></DataTable.Cell> 
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot XYZ</DataTable.Cell> 
-        <DataTable.Cell>{OccupancyXYZCurrent}/{OccupancyXYZMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupancyXYZCurrent,OccupancyXYZMax)} ></DataTable.Cell> 
+        <DataTable.Cell>{OccupationDetermine('Lot XYZ')}/{OccupancyXYZMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot XYZ'),OccupancyXYZMax)} ></DataTable.Cell> 
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot PS1</DataTable.Cell> 
-        <DataTable.Cell>{OccupancyPS1Current}/{OccupancyPS1Max}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupancyPS1Current,OccupancyPS1Max)} ></DataTable.Cell> 
+        <DataTable.Cell>{OccupationDetermine('Lot PS1')}/{OccupancyPS1Max}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot PS1'),OccupancyPS1Max)} ></DataTable.Cell> 
       </DataTable.Row> 
     </DataTable> 
 
