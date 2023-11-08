388ef47c48193eedba048e3d7dfe204c09eacf4b
Jannis <jannisbressan@web.de>
2023-11-07 16:34:16 -0800

    Added Firebase support to the table

--- components/DataTable.js
+++ components/DataTable.js
@@ -1,25 +1,8 @@
-import React from 'react'; 
+import React, { useRef, useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native'; 
 import { DataTable } from 'react-native-paper'; 
+import firebase from '../firebase/firebaseConfig';
 
-/*var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://console.firebase.google.com/u/0/project/parkingbuddy-79e1c/firestore/data/~2FParking%20Structure~2FLot%20F"
-});
-
-const db = admin.firestore();*/
-
-
-OccupancyBCurrent = 200;
-OccupancyCCurrent = 400;
-OccupancyFCurrent = 12;
-OccupancyKCurrent = 160;
-OccupancyXYZCurrent = 312;
-OccupancyPS1Current = 1013;
 OccupancyBMax = 493;
 OccupancyCMax = 535;
 OccupancyFMax = 1288;
@@ -27,34 +10,6 @@ OccupancyKMax = 178;
 OccupancyXYZMax = 816;
 OccupancyPS1Max = 1419;
 
-OccupationDetermine = (Lot) =>{
-
-  //when firebase works in expo use that instead of ifs:
-  //const docRef = db.collection('Parking Structure').doc(Lot);
-  //docRef.get()
-
-  //occupation = doc.data()['OccupationCurrent']
-  if (Lot == 'Lot B'){
-    occupation = 150;
-  }
-  if (Lot == 'Lot C'){
-    occupation = 400;
-  }
-  if (Lot == 'Lot F'){
-    occupation = 643;
-  }
-  if (Lot == 'Lot K'){
-    occupation = 20;
-  }
-  if (Lot == 'Lot XYZ'){
-    occupation = 345;
-  }
-  if (Lot == 'Lot PS1'){
-    occupation = 700;
-  }
-
-  return occupation
-} 
 colorPick = (OccCurrent, OccMax) => {  
   
   if ((OccCurrent/OccMax) <= 0.33){
@@ -70,7 +25,36 @@ colorPick = (OccCurrent, OccMax) => {
     return "green"
   }
 }
+
 const Table = () => { 
+
+const [parkingData, setParkingData] = useState([]);
+
+const findParkingDataById = (documentId) => {
+  const parkingDataItem = parkingData.find((item) => item.id === documentId);
+  return parkingDataItem ? parkingDataItem.occupation : null;
+};
+
+useEffect(() => {
+  // Reference to the "Parking Spaces" collection
+  const parkingRef = firebase.firestore().collection('Parking Structure');
+
+  // Fetch the data
+  parkingRef.get().then((querySnapshot) => {
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      // Access the "Free Spaces" field
+      const occupation = doc.data().OccupationCurrent;
+      data.push({ id: doc.id, occupation});
+    });
+    // Set the retrieved data in the state
+    setParkingData(data);
+  }).catch((error) => {
+    console.error('Error fetching parking data:', error);
+  });
+}, []);
+
+  
   return ( 
     <DataTable style={styles.container}> 
       <DataTable.Header style={styles.tableHeader}> 
@@ -80,34 +64,34 @@ const Table = () => {
       </DataTable.Header> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot B</DataTable.Cell> 
-        <DataTable.Cell>{OccupationDetermine('Lot B')}/{OccupancyBMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot B'),OccupancyBMax)}></DataTable.Cell>  
+        <DataTable.Cell>{findParkingDataById('Lot B')}/{OccupancyBMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(findParkingDataById('Lot B'),OccupancyBMax)}></DataTable.Cell>  
       </DataTable.Row> 
   
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot C</DataTable.Cell> 
-        <DataTable.Cell>{OccupationDetermine('Lot C')}/{OccupancyCMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot C'),OccupancyCMax)} ></DataTable.Cell>   
+        <DataTable.Cell>{findParkingDataById('Lot C')}/{OccupancyCMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(findParkingDataById('Lot C'),OccupancyCMax)} ></DataTable.Cell>   
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot F</DataTable.Cell> 
-        <DataTable.Cell>{OccupationDetermine('Lot F')}/{OccupancyFMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot F'),OccupancyFMax)} ></DataTable.Cell>  
+        <DataTable.Cell>{findParkingDataById('Lot F')}/{OccupancyFMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(findParkingDataById('Lot F'),OccupancyFMax)} ></DataTable.Cell>  
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot K</DataTable.Cell> 
-        <DataTable.Cell>{OccupationDetermine('Lot K')}/{OccupancyKMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot K'),OccupancyKMax)} ></DataTable.Cell> 
+        <DataTable.Cell>{findParkingDataById('Lot K')}/{OccupancyKMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(findParkingDataById('Lot K'),OccupancyKMax)} ></DataTable.Cell> 
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot XYZ</DataTable.Cell> 
-        <DataTable.Cell>{OccupationDetermine('Lot XYZ')}/{OccupancyXYZMax}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot XYZ'),OccupancyXYZMax)} ></DataTable.Cell> 
+        <DataTable.Cell>{findParkingDataById('Lot XYZ')}/{OccupancyXYZMax}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(findParkingDataById('Lot XYZ'),OccupancyXYZMax)} ></DataTable.Cell> 
       </DataTable.Row> 
       <DataTable.Row style={styles.row}> 
         <DataTable.Cell>Lot PS1</DataTable.Cell> 
-        <DataTable.Cell>{OccupationDetermine('Lot PS1')}/{OccupancyPS1Max}</DataTable.Cell> 
-        <DataTable.Cell backgroundColor = {colorPick(OccupationDetermine('Lot PS1'),OccupancyPS1Max)} ></DataTable.Cell> 
+        <DataTable.Cell>{findParkingDataById('Lot PS1')}/{OccupancyPS1Max}</DataTable.Cell> 
+        <DataTable.Cell backgroundColor = {colorPick(findParkingDataById('Lot PS1'),OccupancyPS1Max)} ></DataTable.Cell> 
       </DataTable.Row> 
     </DataTable> 
 
